c2e0edaaaecb3c52c81390f747158abe414a1d62
DenT <sonicspeeds@gmail.com>
2016-07-07 18:16:21 +0000

    exercise 2b done

--- answers.js
+++ answers.js
@@ -2,25 +2,36 @@
 //Write a function called printPositives that takes an array and 
 //uses the forEach method to print only the positive numbers.
 
-var listOfNumbers = [1,-2,4,-9,2,-7,4,-3,9];
+var listOfNumbers = [1, -2, 4, -9, 2, -7, 4, -3, 9];
 
 function printPositives(num) {
-    if (num >0){
+    if (num > 0) {
         console.log(num);
     }
 }
-    
-listOfNumbers.forEach(printPositives);
+
+//var listOfPositiveNumbers = listOfNumbers.forEach(printPositives);
+//console.log(listOfPositiveNumbers);
 
 //Exercise 2
 //Similar to the previous exercise, write a function called getPositives that takes an array
 //and uses the filter method to return a new array with only the positive numbers.
 
 function getPositives(num) {
-    if (num >0){
+    if (num > 0) {
         return num;
     }
 }
 
 var listOfPositiveNumbers = listOfNumbers.filter(getPositives);
 //console.log(listOfPositiveNumbers);
+
+//Exercise 2B
+//Re-do exercise 1 by first filtering the input array,
+//and then printing the numbers from the filtered array. Your code will look something like: `return arr.filter(...).forEach(...)
+
+function printPositivesTheLongWay(arr) {
+    return arr.filter(getPositives).forEach(printPositives);
+}
+
+printPositivesTheLongWay(listOfNumbers);
