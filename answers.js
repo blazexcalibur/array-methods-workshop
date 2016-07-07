80a946b22063f5e6fa0c3dc61b47ed63e7918c9a
DenT <sonicspeeds@gmail.com>
2016-07-07 19:27:46 +0000

    finished excercise 3

--- answers.js
+++ answers.js
@@ -23,7 +23,7 @@ function getPositives(num) {
     }
 }
 
-var listOfPositiveNumbers = listOfNumbers.filter(getPositives);
+//var listOfPositiveNumbers = listOfNumbers.filter(getPositives);
 //console.log(listOfPositiveNumbers);
 
 //Exercise 2B
@@ -34,4 +34,33 @@ function printPositivesTheLongWay(arr) {
     return arr.filter(getPositives).forEach(printPositives);
 }
 
-printPositivesTheLongWay(listOfNumbers);
+//printPositivesTheLongWay(listOfNumbers);
+
+/*
+Exercise 3
+Write a function called filterArray that takes an array AND a function as arguments. 
+Your filter function should return a new array that contains only the elements where the passed function returns a truthy value.
+NOTE: You are allowed to use Array.prototype.filter to answer this question.
+NOTE 2: This is a bit of a trick question, the answer is a one-liner :)
+*/
+
+listOfNumbers = ["dennis", NaN, 4, -9, 2, -7, 4, -3, 9]
+
+function filterArray(arr, funct) {
+    return arr.filter(funct);
+}
+
+function tru(value) {
+    if (value) {
+        return value;
+    }
+}
+
+var listOfPositiveNumbers = filterArray(listOfNumbers, tru);
+console.log(listOfPositiveNumbers);
+
+//Exercise 4
+//Write a function called longestWord that takes a string as argument, and returns the longest word in the string. 
+//You should use Array.prototype.reduce to do your work.
+//Hint: You can use String.prototype.split to split the string into an array of words.
+
