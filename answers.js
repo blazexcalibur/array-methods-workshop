b9f1c8c2744f26cd9b4ce2a9f89752ccd7d32951
DenT <sonicspeeds@gmail.com>
2016-07-08 00:52:14 +0000

    exercise 8 and 9 done

--- answers.js
+++ answers.js
@@ -2,7 +2,7 @@
 //Write a function called printPositives that takes an array and 
 //uses the forEach method to print only the positive numbers.
 
-var listOfNumbers = [1, -2, 4, -9, 2, -7, 4, -3, 9];
+var listOfNumbers = [1, -2, 48, -9, 2, -7, 8, -3, 9];
 
 function printPositives(num) {
     if (num > 0) {
@@ -44,7 +44,7 @@ NOTE: You are allowed to use Array.prototype.filter to answer this question.
 NOTE 2: This is a bit of a trick question, the answer is a one-liner :)
 */
 
-listOfNumbers = ["dennis", NaN, 4, -9, 2, -7, 4, -3, 9]
+//listOfNumbers = ["dennis", NaN, 4, -9, 2, -7, 4, -3, 9]
 
 function filterArray(arr, funct) {
     return arr.filter(funct);
@@ -87,19 +87,19 @@ Hint 2: You can create an array of vowels and use Array.prototype.indexOf to che
 var phraseArray = phrase.split("");
 
 var vowels = phraseArray.reduce(function countVowels(totalV, letter) {
-            var listOfVowels = ["a", "e", "i", "o", "u"];
+    var listOfVowels = ["a", "e", "i", "o", "u"];
 
-            if (listOfVowels.indexOf(letter) > -1) {
-                return totalV + 1;
-            }
-            return totalV;
-        /*
-        if (letter === "a"||letter === "e"||letter === "i"||letter === "o"||letter === "u"){
-            return totalV += 1 ;
-        } 
-        return totalV;
-        */
-    },0);
+    if (listOfVowels.indexOf(letter) > -1) {
+        return totalV + 1;
+    }
+    return totalV;
+    /*
+    if (letter === "a"||letter === "e"||letter === "i"||letter === "o"||letter === "u"){
+        return totalV += 1 ;
+    } 
+    return totalV;
+    */
+}, 0);
 
 //console.log(vowels);
 
@@ -113,20 +113,131 @@ See if you can initialize your reduce accumulator with Infinity and -Infinity :)
 */
 
 
-var highest = listOfNumbers.reduce(function countVowels(highLowCount, num ) {
-    
+var highest = listOfNumbers.reduce(function highLow(highLowCount, num) {
+
     if (num > highLowCount.high) {
         highLowCount.high = num;
     }
-    
+
     if (num < highLowCount.low) {
         highLowCount.low = num;
     }
     return highLowCount;
-}, {high: -Infinity, low: Infinity});
+}, {
+    high: -Infinity,
+    low: Infinity
+});
 
-console.log(highest);
+//console.log(highest);
 
 
 //console.log(phrase.split(""));
+/*
+Exercise 7
+Expanding on exercise 6, write a function called highLowTwo that takes an array of numbers, 
+and returns the higest, second highest, lowest, and second lowest numbers.
+For example, starting with [1, -10, 20, 40, 5], your function should return:
+{
+  "highest": 40,
+  "secondHighest": 20,
+  "lowest": -10,
+  "secondLowest": 5
+}
+*/
+
+listOfNumbers = [1, -10, 20, 40, 5];
+
+var extrema = listOfNumbers.reduce(function highLowTwo(highLowCount, num) {
+
+    if (num > highLowCount.highest) {
+        highLowCount.secondhighest = highLowCount.highest;
+        highLowCount.highest = num;
+    }
+    if (num < highLowCount.highest && num > highLowCount.secondhighest) {
+        highLowCount.secondhighest = num;
+    }
+    if (num < highLowCount.lowest) {
+        highLowCount.secondlowest = highLowCount.lowest;
+        highLowCount.lowest = num;
+    }
+    if (num < highLowCount.secondlowest && num > highLowCount.lowest) {
+        highLowCount.secondlowest = num;
+    }
+    return highLowCount;
+}, {
+    highest: -Infinity,
+    secondhighest: -Infinity,
+    lowest: Infinity,
+    secondlowest: Infinity
+});
+
+//console.log(extrema);
+
+/*
+Exercise 8
+Write a function called countChars that takes a string, and returns an object where the keys are letters, 
+and the value is the number of times that letter appears.
+For example, with input "hello world", the output should be:
+{
+  "h": 1,
+  "e": 1,
+  "l": 3,
+  "o": 2,
+  "w": 1,
+  "r": 1,
+  "d": 1
+}
+NOTE: Unlike arrays, objects don't have any ordering on them. When you print your object on the console, 
+your keys may be displayed in a different order, and it does not matter.
+*/
+
+function countChar(str) {
+    var phraseArray = phrase.split("");
+
+    return phraseArray.reduce(function(countC, letter) {
+
+        if (countC[letter]) {
+        countC[letter] += 1;
+        }
+        else {
+        countC[letter] = 1;
+        }
+        return countC;
+    }, {});
+}
+
+console.log(countChar(phrase));
+
+//Exercise 9
+
+var personaldata = [
+  {
+    "id": "KeXoYg92is",
+    "firstName": "John",
+    "lastName": "Smith",
+    "email": "john@smith.com"
+  },
+  {
+    "id": "NkALmSWtUp",
+    "firstName": "Donald",
+    "lastName": "Duck",
+    "email": "don@disney.com"
+  },
+  {
+    "id": "m7LPbJYSUg",
+    "firstName": "John",
+    "lastName": "Vader",
+    "email": "vader@darkside.com"
+  }
+]
+
+function peopleId(arr) {
+    var browse = arr.reduce(function(person, personkey){
+        person[personkey.id] = personkey;
+        return person;
+    }, {});
+    return browse;
+}
+
+console.log(peopleId(personaldata));
 
